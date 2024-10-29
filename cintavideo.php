27adcc0a6f4d466d0d4db81a78121ee525df394f
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-10-29 10:01:03 +0100

    añadir cinta de video

--- cintavideo.php
+++ cintavideo.php
@@ -3,12 +3,23 @@
 include_once("Soporte.php");
 
 class Cintavideo extends Soporte {
+
     public function __construct(
-        private float $duracion;
-    )
-    
+        string $titulo, 
+        int $numero, 
+        float $precio, 
+        private int $duracion
+    ) {
+        parent::__construct($titulo, $numero, $precio);
+    }
+
+    public function getDuracion(): int {
+        return $this->duracion;
+    }
+
+    public function muestraResumen(): string {
+        return "Precio: " . $this->getPrecio() . ", precio con IVA: " . $this->getPrecioConIVA() . ", título: " . $this->titulo . ", duración: " . $this->duracion . " minutos";
+    }
 }
-
-
 
 ?>
--- inicio.php
+++ inicio.php
@@ -9,10 +9,11 @@ include "Soporte.php";
     echo $soporte1->muestraResumen();
 
 include "cintavideo.php";
+
     $miCinta = new Cintavideo("Los cazafantasmas", 23, 3.5, 107);
-    echo "<strong>" . $miCinta->titulo . "</strong>";
+    echo "<br><br><strong>" . $miCinta->titulo . "</strong>";
     echo "<br>Precio: " . $miCinta->getPrecio() . " euros";
     echo "<br>Precio IVA incluido: " . $miCinta->getPrecioConIva() . " euros";
-    $miCinta->muestraResumen();
+    echo $miCinta->muestraResumen();
 
 ?>
