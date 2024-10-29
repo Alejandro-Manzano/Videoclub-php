01e97ca4d8b09adc94c007617762ae2bf34e7524
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-10-29 09:29:32 +0100

    cintavideo

---
+++ cintavideo.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once("Soporte.php");
+
+class Cintavideo extends Soporte {
+    public function __construct(
+        private float $duracion;
+    )
+    
+}
+
+
+
+?>
--- inicio.php
+++ inicio.php
@@ -8,4 +8,11 @@ include "Soporte.php";
     echo "<br>Precio IVA incluido: " . $soporte1->getPrecioConIVA() . " euros <br>";
     echo $soporte1->muestraResumen();
 
+include "cintavideo.php";
+    $miCinta = new Cintavideo("Los cazafantasmas", 23, 3.5, 107);
+    echo "<strong>" . $miCinta->titulo . "</strong>";
+    echo "<br>Precio: " . $miCinta->getPrecio() . " euros";
+    echo "<br>Precio IVA incluido: " . $miCinta->getPrecioConIva() . " euros";
+    $miCinta->muestraResumen();
+
 ?>
